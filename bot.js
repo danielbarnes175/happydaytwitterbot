42cddecd196928940f123b3884ca0405eb7db8ff
Daniel Barnes <31489642+danielbarnes175@users.noreply.github.com>
2019-03-13 15:56:21 -0500

    Initial commit

---
+++ bot.js
@@ -0,0 +1,54 @@
+const botSettings = require("./botSettings.json");
+const twit = require("twit");
+const fs = require("fs");
+
+//text1 is the list of adjectives
+//text2 is the list of nouns
+//text3 is the list of abstract nouns
+const text1 = fs.readFileSync("./words/adjectivelist.txt").toString('utf-8');
+const text2 = fs.readFileSync("./words/nounlist.txt").toString('utf-8');
+const text3 = fs.readFileSync("./words/abstractnounlist.txt").toString('utf-8');
+
+let timeBetweenTweets = 1000 * 60 * 60 * 24;
+//let timeBetweenTweets = 60000;
+
+let Twitter = new twit(botSettings);
+console.log("Bot online!");
+
+
+let tweet_day = function() {
+	//Find the adjective to use
+	var adjByLine = text1.split("\n");
+	var whichAdj = Math.floor(Math.random() * adjByLine.length);
+	var adj = adjByLine[whichAdj];
+	adj = (adj.charAt(0).toUpperCase() + adj.slice(1)).trim();
+
+	//Find the noun to use
+	var textByLine = text2.split("\n")
+	var whichNoun = Math.floor(Math.random() * textByLine.length);
+	var noun = textByLine[whichNoun];
+	noun = (noun.charAt(0).toUpperCase() + noun.slice(1)).trim();
+
+	//Find the abstract noun to use
+	var abstractByLine = text3.split("\n");
+	var whichAbs = Math.floor(Math.random() * abstractByLine.length);
+	var abstract = abstractByLine[whichAbs];
+	abstract = abstract.trim();
+	abstract = (abstract.charAt(0).toUpperCase() + abstract.slice(1)).trim();
+
+	let tweet = `Happy ${adj} ${noun} ${abstract} Day!`;
+
+	Twitter.post('statuses/update', { status: tweet}, function(err, data, response) {
+		if (err) {
+			console.log(err);
+		} else {
+			//console.log(data);
+			console.log("Successfully tweeted!");
+		}
+	});
+}
+
+tweet_day();
+
+
+setInterval(tweet_day, timeBetweenTweets);
